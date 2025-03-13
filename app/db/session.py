5d10b41bdb2db7ff8f460c6269948b0a007e1c6e
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2025-03-13 16:15:21 +0300

    fix get db

--- app/db/session.py
+++ app/db/session.py
@@ -4,3 +4,9 @@ from app.core.config import settings
 
 engine = create_async_engine(settings.DATABASE_URL, echo=True)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+# Dependency for database session.
+async def get_db():
+    async with async_session() as session:
+        yield session
