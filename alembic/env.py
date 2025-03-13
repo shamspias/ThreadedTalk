30a6b899bac249d69b3af5db3139d530438db912
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2025-03-13 15:23:42 +0300

    fix: env with db

---
+++ alembic/env.py
@@ -0,0 +1,50 @@
+import os
+from logging.config import fileConfig
+
+from sqlalchemy import engine_from_config, create_engine, pool
+from alembic import context
+
+# Import your base metadata
+from app.database.models.base import Base
+from app.services.config import SYNC_DATABASE_URL
+
+# This is the Alembic Config object
+config = context.config
+
+# Interpret the config file for Python logging.
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+# Set your metadata here
+target_metadata = Base.metadata
+
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode."""
+    # Provide the URL for 'offline' migrations:
+    url = SYNC_DATABASE_URL
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    """Run migrations in 'online' mode."""
+    # Create sync engine from SYNC_DATABASE_URL
+    connectable = create_engine(SYNC_DATABASE_URL, poolclass=pool.NullPool)
+
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata)
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
