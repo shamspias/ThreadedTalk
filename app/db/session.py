81ab8f6776bbb3bf585f01b1b9149d050a852e5f
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2025-03-13 16:07:36 +0300

    fix session for thread

---
+++ app/db/session.py
@@ -0,0 +1,6 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from app.core.config import settings
+
+engine = create_async_engine(settings.DATABASE_URL, echo=True)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
